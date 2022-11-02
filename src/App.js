343eb7d058ad433bee6a26b6e0a6af3ea2927517
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-11-01 23:23:15 -0500

    MERN crear autor y mostrar autores

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import NuevoAutor from './componentes/NuevoAutor';
+import TodosAutores from './componentes/TodosAutores';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+  <div className="container">
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={()=> <TodosAutores/>} />
+        <Route path="/nuevo" exact render={()=> <NuevoAutor/>} />
+      </Switch>
+    </BrowserRouter>
+  </div>
+  )
 }
 
 export default App;

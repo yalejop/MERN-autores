c57dc9a5764670a8a8c33d04797b9cf5e78713cd
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-11-20 21:28:36 -0500

    login y registro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActualizarAutor from './componentes/ActualizarAutor';
 import Error from './componentes/Error';
+import LoginRegistro from './componentes/LoginRegistro';
 import NuevoAutor from './componentes/NuevoAutor';
 import TodosAutores from './componentes/TodosAutores';
 
@@ -9,6 +10,7 @@ const App = () => {
   <div className="container">
     <BrowserRouter>
       <Switch>
+        <Route path="/login" render={()=> <LoginRegistro/> } />
         <Route path="/" exact render={()=> <TodosAutores/>} />
         <Route path="/nuevo" exact render={()=> <NuevoAutor/>} />
         <Route path="/autor/editar/:id" exact render={()=> <ActualizarAutor/>} />
